fdb2ad3e92b1cae5ecaa297effa15322a9eb9469
Rodolphe-M <rodolphe.montaigut@gmail.com>
2023-08-29 11:47:40 +0200

    add mixin collapse and add wrapper logement

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -3,37 +3,41 @@ import data from '../../utils/data/dataAbout.json'
 import Chevron from '../../assets/chevron.svg'
 
 function Collapse() {
-  const [selected, setSelected] = useState(null)
+    const [selected, setSelected] = useState(null)
 
-  const toggle = (i) => {
-    if (selected === i) {
-      return setSelected(null)
+    const toggle = (i) => {
+        if (selected === i) {
+            return setSelected(null)
+        }
+        setSelected(i)
     }
-    setSelected(i)
-  }
 
-  return (
-    <div className="wrapper">
-      {data.map((item, i) => (
-        <div className="wrapper__about" key={i}>
-          <div className="wrapper__about__title">
-            <h5>{item.title}</h5>
-            <img
-              onClick={() => toggle(i)}
-              className={`chevron-icon ${selected === i ? 'active' : ''}`}
-              src={Chevron}
-              alt="chevron-logo"
-            />
-          </div>
-          <div
-            className={`wrapper__about__text ${selected === i ? 'active' : ''}`}
-          >
-            {item.text}
-          </div>
+    return (
+        <div className="wrapper" id="wrapper">
+            {data.map((item, i) => (
+                <div className="wrapper__collapse" key={i}>
+                    <div className="wrapper__collapse__title">
+                        <h5>{item.title}</h5>
+                        <img
+                            onClick={() => toggle(i)}
+                            className={`chevron-icon ${
+                                selected === i ? 'active' : ''
+                            }`}
+                            src={Chevron}
+                            alt="chevron-logo"
+                        />
+                    </div>
+                    <div
+                        className={`wrapper__collapse__text ${
+                            selected === i ? 'active' : ''
+                        }`}
+                    >
+                        {item.text}
+                    </div>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default Collapse
--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import { useState, React } from 'react'
 import { useParams } from 'react-router-dom'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import data from '../../utils/data/data.json'
+import Chevron from '../../assets/chevron.svg'
 import Slideshow from '../../components/Slideshow'
+import data from '../../utils/data/data.json'
 
 const Logement = () => {
     const { id } = useParams()
     const logement = data.find((logement) => logement.id === id)
 
-    const rating = parseInt(logement.rating) // Convertir la note en nombre
+    const rating = parseInt(logement.rating)
 
     const starArray = Array.from({ length: 5 }, (_, index) => (
         <FontAwesomeIcon
@@ -18,6 +19,17 @@ const Logement = () => {
             className={index < rating ? 'star-active' : 'star-inactive'}
         />
     ))
+
+    const [selectedDescription, setSelectedDescription] = useState(false)
+    const [selectedEquipment, setSelectedEquipment] = useState(false)
+
+    const toggleDescription = () => {
+        setSelectedDescription(!selectedDescription)
+    }
+
+    const toggleEquipment = () => {
+        setSelectedEquipment(!selectedEquipment)
+    }
 
     return (
         <div className="container-logement">
@@ -40,6 +52,60 @@ const Logement = () => {
                         <img src={logement.host.picture} alt="owner" />
                     </div>
                     <div className="rating">{starArray}</div>
+                </div>
+            </div>
+
+            <div className="container-wrapper">
+                {/* Collapse DESCRIPTION */}
+                <div className="wrapper">
+                    <div className="wrapper__collapse">
+                        <div className="wrapper__collapse__title">
+                            <h5>Description</h5>
+                            <img
+                                onClick={toggleDescription}
+                                className={`chevron-icon ${
+                                    selectedDescription ? 'active' : ''
+                                }`}
+                                src={Chevron}
+                                alt="chevron-logo"
+                            />
+                        </div>
+                        <div
+                            className={`wrapper__collapse__text ${
+                                selectedDescription ? 'active' : ''
+                            }`}
+                        >
+                            {logement.description}
+                        </div>
+                    </div>
+                </div>
+
+                {/* Collapse EQUIPMENT */}
+                <div className="wrapper">
+                    <div className="wrapper__collapse">
+                        <div className="wrapper__collapse__title">
+                            <h5>Équipement</h5>
+                            <img
+                                onClick={toggleEquipment}
+                                className={`chevron-icon ${
+                                    selectedEquipment ? 'active' : ''
+                                }`}
+                                src={Chevron}
+                                alt="chevron-logo"
+                            />
+                        </div>
+                        <div
+                            className={`wrapper__collapse__text ${
+                                selectedEquipment ? 'active' : ''
+                            }`}
+                        >
+                            <ul>
+                                {logement.equipments.map((equipment, index) => (
+                                    <li key={index}>{equipment}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
